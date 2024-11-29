fd5f31646e1e3db6d5014089761abc23776c4e34
sadik-fattah <sadikfattah5@gmail.com>
2024-11-29 23:13:47 +0100

    add progressbar save result

---
+++ app/src/main/java/com/guercifzone/photorecovrie/ImageSaver.java
@@ -0,0 +1,75 @@
+package com.guercifzone.photorecovrie;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ImageSaver {
+    public static  void SaveImageToGallery(Context context, ImageView imageView){
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            saveImageToMediaStore(context,bitmap);
+        }else {
+            SaveImageToFileSystem(context,bitmap);
+        }
+    }
+
+    private static void saveImageToMediaStore(Context context, Bitmap bitmap) {
+        try {
+            // Create a ContentValues object to describe the image
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, "Image_" + System.currentTimeMillis() + ".png");
+            contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/ImageSaver");
+
+            // Insert the image into the MediaStore and get a Uri
+            Uri imageUri = context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+            // Check if Uri is not null
+            if (imageUri != null) {
+                // Get OutputStream from the Uri
+                try (OutputStream outputStream = context.getContentResolver().openOutputStream(imageUri)) {
+                    if (outputStream != null) {
+                        // Compress and write the bitmap to the output stream
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                        Log.d("ImageSaver", "Image saved successfully!");
+                        Toast.makeText(context, "Image saved successfully!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    Log.e("ImageSaver", "Error saving image", e);
+                    Toast.makeText(context, "Error saving image to MediaStore", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } catch (Exception e) {
+            Log.e("ImageSaver", "Error saving image to MediaStore", e);
+            Toast.makeText(context, "Error saving image to MediaStore", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private static void SaveImageToFileSystem(Context context, Bitmap bitmap) {
+        try {
+            String fileName = "image_" +System.currentTimeMillis() + ".png";
+            FileOutputStream outputStream  = context.openFileOutput(fileName,context.MODE_PRIVATE);
+            bitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream);
+            outputStream.flush();
+            outputStream.close();
+            Log.d("ImageSaver","Image saved successfully!");
+        }catch (IOException e){
+            Log.e("ImageSaver", "Error Saving image",e);
+
+        }
+    }
+}
--- app/src/main/java/com/guercifzone/photorecovrie/MainActivity.java
+++ app/src/main/java/com/guercifzone/photorecovrie/MainActivity.java
@@ -1,26 +1,43 @@
 package com.guercifzone.photorecovrie;
 
+
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
 import java.io.FileNotFoundException;
 
+
 public class MainActivity extends AppCompatActivity {
     private static final int PICK_IMAGE = 1;
     private ImageView imageView;
-    private Button loadImageBtn, invertImageBtn;
+    private Button loadImageBtn, invertImageBtn,saveImageBtn;
+    private ProgressBar progressBar;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         loadImageBtn = findViewById(R.id.loadImageBtn);
         invertImageBtn = findViewById(R.id.invertImageBtn);
+        saveImageBtn =  findViewById(R.id.saveImageBtn);
+       progressBar = findViewById(R.id.progressBar);
 
         loadImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 openGallery();
             }
         });
-        invertImageBtn.setOnClickListener(new View.OnClickListener() {
+       /* invertImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (imageView.getDrawable() != null) {
@@ -45,7 +64,56 @@ public class MainActivity extends AppCompatActivity {
                     imageView.setImageBitmap(invertedBitmap);
                 }
             }
+        });*/
+        invertImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show the ProgressBar
+                progressBar.setVisibility(View.VISIBLE);
+
+                // Check if there's a drawable in the imageView
+                if (imageView.getDrawable() != null) {
+                    Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+
+                    // Perform the image inversion in a separate thread to avoid blocking the UI
+                    new AsyncTask<Bitmap, Void, Bitmap>() {
+                        @Override
+                        protected Bitmap doInBackground(Bitmap... params) {
+                            // Invert the image in the background thread
+                            return ImageProcessing.invertImage(params[0]);
+                        }
+
+                        @Override
+                        protected void onPostExecute(Bitmap invertedBitmap) {
+                            // Hide the ProgressBar once the image is processed
+                            progressBar.setVisibility(View.GONE);
+
+                            // Set the inverted image on the imageView
+                            imageView.setImageBitmap(invertedBitmap);
+                        }
+                    }.execute(bitmap);
+                }
+            }
         });
+
+        saveImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            1);
+                } else {
+                    // Permission already granted, you can call the save function here
+                    ImageSaver.SaveImageToGallery(MainActivity.this, imageView);
+                    Toast.makeText(getApplicationContext(), "Image saved successfully!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
     }
 
     private void openGallery() {
